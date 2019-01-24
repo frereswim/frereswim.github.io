25d8e3ea48a0841919e4689076351338b48713c0
frereswim <xieyong@do1.com.cn>
2019-01-24 14:32:46 +0800

    20190124

---
+++ js/tianqi.js
@@ -0,0 +1,15 @@
+(function(T,h,i,n,k,P,a,g,e){g=function(){P=h.createElement(i);a=h.getElementsByTagName(i)[0];P.src=k;P.charset="utf-8";P.async=1;a.parentNode.insertBefore(P,a)};T["ThinkPageWeatherWidgetObject"]=n;T[n]||(T[n]=function(){(T[n].q=T[n].q||[]).push(arguments)});T[n].l=+new Date();if(T.attachEvent){T.attachEvent("onload",g)}else{T.addEventListener("load",g,false)}}(window,document,"script","tpwidget","//widget.seniverse.com/widget/chameleon.js"))
+
+tpwidget("init", {
+    "flavor": "bubble",
+    "location": "WS0E9D8WN298",
+    "geolocation": "enabled",
+    "position": "bottom-left",
+    "margin": "10px 10px",
+    "language": "zh-chs",
+    "unit": "c",
+    "theme": "chameleon",
+    "uid": "UDFBB8133E",
+    "hash": "bf93c97c62d0632e97b47ed5f90b0760"
+});
+tpwidget("show");
